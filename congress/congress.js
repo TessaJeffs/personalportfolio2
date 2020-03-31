70421bfccc028af5bae20df2da1cb0254ab43bc4
Tessa Jeffs <just.tes17@gmail.com>
2020-03-31 10:36:34 -0600

    congress

--- congress/congress.js
+++ congress/congress.js
@@ -1,12 +1,31 @@
-import { senators } from '../data/senators.js'
+import { senators } from '../data/senators'
 const container = document.querySelector ('.container')
 
+
+// filter party data of senators
 const filterSenators = (prop, value) => {
     return senators.filter (senators => senator[prop] === value)
 }
 
+
+function populateContainer() {
+    senators.forEach(senator => {
+        let middleName = senator.middle_name ? 
+        ` ${senator.middle_name} ` : ` `
+        let senPara = document.createElement('p')
+        senPara.textContent = 
+        `${senator.first_name} ${middleName} ${senator.last_name}`  
+    container.appendChild (senPara) 
+})
+}
+
+populateContainer()
+
+
 console.log(filterSenators('party', 'R'))
-
 console.log(filterSenators('party', 'D'))
-
 console.log(filterSenators('party', 'ID'))
+
+
+// map
+
